d7bf0ad340814e8be04b7f254af5b89d11e858f0
Jean <jeanwill93@hotmail.com>
2024-04-09 12:03:42 +0100

    confg css pag about

--- src/Pages/Home/Sections/About/About.tsx
+++ src/Pages/Home/Sections/About/About.tsx
@@ -1,68 +1,54 @@
-import { Typography,styled } from "@mui/material";
+import { Typography, Card, CardActionArea, CardMedia, CardContent } from "@mui/material";
 import Avatar from "../../../../assets/Images/avatar.jpg";
+import "./About.css";
 
-
-
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import { CardActionArea } from '@mui/material';
-
-
-
-
-const About= () => {
-
-  
-  const StyledAbout = styled("div")(({ theme }) => ({
-    backgroundColor: theme.palette.secondary.main,
-    height: "100vh",
-    display: "flex",
-    alignItems: "center",
-    [theme.breakpoints.up("xs")]: {
-      //<=mobile
-      paddingTop: "0px",
-    },
-    [theme.breakpoints.up("md")]: {
-      //=> mobile
-    },
-  }));
-
-    
-    
-    return (
-      <>
-      <StyledAbout>
+const About = () => {
+  return (
+    <>
       <div className="container">
-        <div className="elemento" >
-        <Card sx={{ maxWidth: 300,  }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="180"
-          src={Avatar}/>
-          
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Jean Will
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            I Have some academic Experience in Js, React in another tecnologies
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-
-
+        <div className="elemento card-animation">
+          <Card sx={{ maxWidth: 300 }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="180"
+                image={Avatar}
+                alt="Avatar"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Jean Will
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  I Have some academic Experience in Js, React in another technologies
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </div>
+        <div className="elemento card-animation">
+          <Card sx={{ maxWidth: 300 }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="180"
+                image={Avatar}
+                alt="Avatar"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Education
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  I Studing technologies in Universidade Cidade de São Paulo (UNICID)
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
         </div>
       </div>
+    </>
+  );
+};
 
-      </StyledAbout>
-      
-     
-      
-      </>
-    )
-  }
-   export default About;
-  
+export default About;
