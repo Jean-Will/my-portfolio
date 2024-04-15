a7c42dd98879941b01cc1fe5a2e3ad758637de22
Jean <jeanwill93@hotmail.com>
2024-04-15 12:19:07 +0100

    classname card-content add, included text about

--- src/Pages/Home/Sections/About/About.tsx
+++ src/Pages/Home/Sections/About/About.tsx
@@ -5,15 +5,18 @@ import "./About.css";
 
 
 
+
 const About = () => {
 
   const StyledAbout = styled("div")(({ theme }) => ({
+   
     
     [theme.breakpoints.up("xs")]: {
+      flexDirection: "row",
       
     },
     [theme.breakpoints.up("md")]: {
-      
+      flexDirection: "row", 
     },
   }));
 
@@ -23,7 +26,7 @@ const About = () => {
       <div className="container">
         <div className="elemento card-animation">
           
-          <Card sx={{ maxWidth: 300 ,  }}>
+          <Card sx={{ maxWidth: 300 ,}}>
             <CardActionArea>
               <CardMedia
                 component="img"
@@ -31,12 +34,15 @@ const About = () => {
                 image={Avatar}
                 alt="Avatar"
               />
-              <CardContent>
+              <CardContent className="card-content">
                 <Typography gutterBottom variant="h5" component="div">
                   Jean Will
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                  I Have some academic Experience in Js, React in another technologies
+                I have some academic experience in JavaScript, React.Js, Angular.Js, this is only about FrontEnd.
+                Now in About BackEnd I have used C#.NET sometimes and SQL.
+               I have published my codes in GitHub since my first code, 
+               so you can see my progress during this time and follow my skills developed and compare my first project with this project. Enjoy this journeys
                 </Typography>
               </CardContent>
             </CardActionArea>
@@ -51,12 +57,16 @@ const About = () => {
                 image={Avatar}
                 alt="Avatar"
               />
-              <CardContent>
+              <CardContent className="card-content">
                 <Typography gutterBottom variant="h5" component="div">
                   Education
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                  I Studing technologies in Universidade Cidade de São Paulo (UNICID)
+                I'm studying Systems Analysis and Development at Universidade Cidade de São Paulo (UNICID),
+                 where I took some tests and my evaluations were very good.
+                I know I'm starting my studies, but I study every day, but not only at my university,
+                 I have used some platforms like Microsoft Learn, Youtube, Google, Udemy among others.
+                I have studied a little about SCRUM agile methodologies where I have certification.
                 </Typography>
               </CardContent>
             </CardActionArea>
