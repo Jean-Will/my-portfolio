14b17370bd2eb2277fdb92397439575372e6ce28
Jean <jeanwill93@hotmail.com>
2024-06-07 13:34:02 +0100

    change pag about, the row to colum

--- src/Pages/Home/Sections/About/About.tsx
+++ src/Pages/Home/Sections/About/About.tsx
@@ -1,32 +1,25 @@
-import { Typography, Card, CardActionArea, CardMedia, CardContent , styled } from "@mui/material";
+import { Typography, Card, CardActionArea, CardMedia, CardContent, styled } from "@mui/material";
 import Avatar from "../../../../assets/Images/avatar.jpg";
 import "./About.css";
 
+const StyledAbout = styled("div")(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: theme.spacing(2),
 
-
-
+  [theme.breakpoints.up("md")]: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+}));
 
 const About = () => {
-
-  const StyledAbout = styled("div")(({ theme }) => ({
-   
-    
-    [theme.breakpoints.up("xs")]: {
-      flexDirection: "row",
-      
-    },
-    [theme.breakpoints.up("md")]: {
-      flexDirection: "row", 
-    },
-  }));
-
   return (
-    <>
     <StyledAbout>
       <div className="container">
         <div className="elemento card-animation">
-          
-          <Card sx={{ maxWidth: 300 ,}}>
+          <Card className="card">
             <CardActionArea>
               <CardMedia
                 component="img"
@@ -39,17 +32,17 @@ const About = () => {
                   Jean Will
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                I have some academic experience in JavaScript, React.Js, Angular.Js, this is only about FrontEnd.
-                Now in About BackEnd I have used C#.NET sometimes and SQL.
-               I have published my codes in GitHub since my first code, 
-               so you can see my progress during this time and follow my skills developed and compare my first project with this project. Enjoy this journeys
+                  I have some academic experience in JavaScript, React.Js, Angular.Js, this is only about FrontEnd.
+                  Now in About BackEnd I have used C#.NET sometimes and SQL.
+                  I have published my codes in GitHub since my first code,
+                  so you can see my progress during this time and follow my skills developed and compare my first project with this project. Enjoy this journeys
                 </Typography>
               </CardContent>
             </CardActionArea>
           </Card>
         </div>
         <div className="elemento card-animation">
-          <Card sx={{ maxWidth: 300, }}>
+          <Card className="card">
             <CardActionArea>
               <CardMedia
                 component="img"
@@ -62,19 +55,18 @@ const About = () => {
                   Education
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                I'm studying Systems Analysis and Development at Universidade Cidade de São Paulo (UNICID),
-                 where I took some tests and my evaluations were very good.
-                I know I'm starting my studies, but I study every day, but not only at my university,
-                 I have used some platforms like Microsoft Learn, Youtube, Google, Udemy among others.
-                I have studied a little about SCRUM agile methodologies where I have certification.
+                  I'm studying Systems Analysis and Development at Universidade Cidade de São Paulo (UNICID),
+                  where I took some tests and my evaluations were very good.
+                  I know I'm starting my studies, but I study every day, but not only at my university,
+                  I have used some platforms like Microsoft Learn, Youtube, Google, Udemy among others.
+                  I have studied a little about SCRUM agile methodologies where I have certification.
                 </Typography>
               </CardContent>
             </CardActionArea>
           </Card>
         </div>
       </div>
-      </StyledAbout>
-    </>
+    </StyledAbout>
   );
 };
 
